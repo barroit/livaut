6ee8ba44692af1ba91eee1989b8eb0e2c08adc51
Jiamu Sun <barroit@linux.com>
2024-06-20 01:24:10 +0900

    br: initialize project structure
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ src/livaut.c
@@ -0,0 +1,25 @@
+/****************************************************************************
+**
+** Copyright 2024 Jiamu Sun
+** Contact: barroit@linux.com
+**
+** This file is part of livaut.
+**
+** livaut is free software: you can redistribute it and/or modify it
+** under the terms of the GNU General Public License as published by the
+** Free Software Foundation, either version 3 of the License, or (at your
+** option) any later version.
+**
+** livaut is distributed in the hope that it will be useful, but
+** WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along
+** with livaut. If not, see <https://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+void app_main(void)
+{
+}
